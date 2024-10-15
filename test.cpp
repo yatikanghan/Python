cbc6778f36345c71d45e9ca315c374d779858395
yatikanghan <yatikanghan883@gmail.com>
2024-10-15 17:09:34 +0200

    test

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{      
+    string username;
+    cout <<"What is your name?" << std::endl;
+    cin >> username;
+    cout <<"Welcome "<< username << " to Python Programming";
+
+    return 0;
+}
---
+++ test.py
@@ -0,0 +1,13 @@
+""" 
+multi line
+comment
+"""
+
+# single line comment
+
+print("Hello Word!")
+
+username=input("Enter the your name : ")
+print("Welcome "+ username + " to Python Programming!")
+
+
