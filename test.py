73c7ad1432a34bd4c1a7e9956fdc99e76341339d
yatikanghan <yatikanghan883@gmail.com>
2024-10-16 11:34:44 +0200

    date update

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ username=input("Enter the your name : ")
 print("Welcome "+ username + " to Python Programming!")
 
 print("this is a extra message for testing")
+print("16-oct 2024")
 
 print("Goodbye "+username)
 print("Thank you")
